c50d231f485e01259600f3439ebc5de20175848f
とろろんぴ <ntmsk@tororonpinoMacBook-ea-2.local>
2020-07-02 07:31:12 +0900

    add toppage

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify' // ←ここに追加
+
+Vue.config.productionTip = false
+
+new Vue({
+  vuetify, // ←ここに追加
+  render: h => h(App),
+}).$mount('#app')
